8b3be8b32be38cb3649a75a515fec677d924b3b6
maddealer <forento@gmail.com>
2020-04-13 17:48:25 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+app.use(cookieParser());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb://localhost:27017/mernauth",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("MongoDB connected ;)");
+  }
+);
+
+app.listen(5000, () => {
+  console.log("Server started ;)");
+});
