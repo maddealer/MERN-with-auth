66e7cfd8fcea8d9cf14ff1ffbf81099247b18815
maddealer <forento@gmail.com>
2020-04-15 18:07:19 +0300

    routes done

--- server/routes/User.js
+++ server/routes/User.js
@@ -6,6 +6,17 @@ const JWT = require("jsonwebtoken");
 const User = require("../models/User");
 const Todo = require("../models/Todo");
 
+const signToken = (userId) => {
+  return JWT.sign(
+    {
+      iss: "maddealer",
+      sub: userId,
+    },
+    "maddealer",
+    { expiresIn: "1h" }
+  );
+};
+
 userRouter.post("/register", (req, res) => {
   const { username, password, role } = req.body;
 
@@ -38,5 +49,105 @@ userRouter.post("/register", (req, res) => {
     }
   });
 });
+
+userRouter.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      const { _id, username, role } = req.user;
+      const token = signToken(_id);
+      res.cookie("access_token", token, { httpOnly: true, sameSite: true });
+      res.status(200).json({ isAuthenticated: true, user: { username, role } });
+    }
+  }
+);
+
+userRouter.get(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.clearCookie("access_token");
+    res.json({ user: { username: "", role: "" }, success: true });
+  }
+);
+
+userRouter.post(
+  "/todo",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const todo = new Todo(req.body);
+
+    todo.save((err) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ message: { msgBody: "Error has occured", msgError: true } });
+      } else {
+        req.user.todos.push(todo);
+        req.user.save((err) => {
+          if (err) {
+            res.status(500).json({
+              message: { msgBody: "Error has occured", msgError: true },
+            });
+          } else {
+            res.status(200).json({
+              message: {
+                msgBody: "Successfully created todo",
+                msgError: false,
+              },
+            });
+          }
+        });
+      }
+    });
+  }
+);
+
+userRouter.get(
+  "/todos",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findById({ _id: req.user._id })
+      .populate("todos")
+      .exec((err, document) => {
+        if (err) {
+          res.status(500).json({
+            message: { msgBody: "Error has occured", msgError: true },
+          });
+        } else {
+          res.status(200).json({
+            todos: document.todos,
+            authenticated: true,
+          });
+        }
+      });
+  }
+);
+
+userRouter.get(
+  "/admin",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.role === "admin") {
+      res
+        .status(200)
+        .json({ message: { msgBody: "You are an admin", msgError: false } });
+    } else {
+      res.status(403).json({
+        message: { msgBody: "You are not an admin, go away", msgError: true },
+      });
+    }
+  }
+);
+
+userRouter.get(
+  "/authenticated",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+  }
+);
 
 module.exports = userRouter;
