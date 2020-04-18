f1e33aac88ad1a4f60b4e8c3c0cf623cfda634c1
maddealer <forento@gmail.com>
2020-04-18 19:54:18 +0300

    navbar done more

--- client-react/src/Components/Navbar.js
+++ client-react/src/Components/Navbar.js
@@ -59,15 +59,25 @@ const Navbar = (props) => {
   };
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/">
         <div className="navbar-brand">maddealer</div>
       </Link>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
           {!isAuthenticated ? unauthentcatedNavbar() : authenticatedNavbar()}
         </ul>
-        <span class="navbar-text">Navbar text with an inline element</span>
       </div>
     </nav>
   );
